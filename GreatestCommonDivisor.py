315a679ab43fde9778882fa0e85239332b1def02
Talin Estiban <124513647+Talin-Estiban@users.noreply.github.com>
2023-10-26 13:17:51 +0300

    adding comments

--- GreatestCommonDivisor.py
+++ GreatestCommonDivisor.py
@@ -5,18 +5,18 @@ def gcdOfStrings(self, str1, str2):
         :type str2: str
         :rtype: str
         """
-        if len(str1)>len(str2):
+        if len(str1)>len(str2):// saving the shorter word as the base
             base = str2
             word=str1
         else:
             base = str1
             word = str2
         for i in range(len(base)):
-            length = len(word) / len(base)
+            length = len(word) / len(base) //the ratio between the word and the base
             sample=""
-            for j in range(length):
+            for j in range(length): // concatenate the base ratio times
                 sample += base
-            if sample==word:
+            if sample==word: //check if we created the word by concatenating the base
                 return(base)
             else:
                 base = base[:-1]
--- MergeStringsAlternately.py
+++ MergeStringsAlternately.py
@@ -8,19 +8,21 @@ def mergeAlternately(self, word1, word2):
         :rtype: str
         """
         word=""
+	// check if one word is longer that the other
         length = len(word1)-len(word2)
         if length==0:
+	    // add letters from both words alternatvely
             for i in range(len(word1)):
                 word += (word1[i]+word2[i])
         else:
             if length>0:
                 for i in range(len(word2)):
                     word += (word1[i]+word2[i])
-                word += word1[length:]
+                word += word1[length:] //word1 is longer--> add the rest to the end
             else:
                 for i in range(len(word1)):
                     word += (word1[i]+word2[i])
-                word += word2[length:]
+                word += word2[length:]//word2 is longer--> add the rest to the end
         return(word)
 
 #second approach- 19ms runtime
@@ -31,7 +33,7 @@ def mergeAlternately(self, word1, word2):
         :rtype: str
         """
         word = ""
-        index = max(len(word1),len(word2))
+        index = max(len(word1),len(word2)) // figure out which word is longer
         i = 0
         while i<index: 
             if i<len(word1):
