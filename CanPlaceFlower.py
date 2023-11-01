102a8897da0fd8d1ce4cc740d56b3c0917d5880e
Talin Estiban <124513647+Talin-Estiban@users.noreply.github.com>
2023-11-01 21:37:06 +0200

    Add strategy and problem description

--- CanPlaceFlower.py
+++ CanPlaceFlower.py
@@ -1,5 +1,25 @@
 # Can Place Flower 
+"""
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
 
+Strategy:
+first intiution:
+- initiate a parameter x to store the number of flowers we can plant. 
+- we iterate through the flowerbed list:
+	- on the first value if it's 0--> increment x.
+	- in the middle values of the flowerbed if there are 3 corresponding zeros --> increment x.
+	- for the last value if it's 0--> increment x.
+- compare x and n.
+
+second answer:
+- like the first intuition we create a parameter x to store the number of flowers we can plant. 
+- iterate through the list:
+	- check the right side if it's zero or it's the first value.
+	- check the left side if it's zero or it;s the last value.
+	- if the two previous conditions are met --> increment x.
+- compare x and n.
+"""
 # first intuition- runtime 33ms 
     def canPlaceFlowers(self, flowerbed, n):
         """
