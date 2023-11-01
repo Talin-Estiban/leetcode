5935940db1edd493cd8732e2115271fa32e539cc
Talin Estiban <124513647+Talin-Estiban@users.noreply.github.com>
2023-11-01 19:54:31 +0200

    adding strategy and problem description

--- GreatestCommonDivisor.py
+++ GreatestCommonDivisor.py
@@ -1,4 +1,13 @@
 # greatest common divisor of strings- runtime 30ms
+"""
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+Strategy:
+- take the smaller string as a base.
+- get the ratio ratio of the lengths of the string (longer one) and the base.
+- if the base is concatinated with itself by the "ratio" times and the result is the string then the base is the greatest common divisor.
+"""
 def gcdOfStrings(self, str1, str2):
         """
         :type str1: str
